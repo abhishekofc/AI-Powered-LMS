55c134217aa386f1e0b9da7c351fcc35600b9161
Abhishek Pandey <abhishek.pandey.2k05@gmail.com>
2025-08-16 21:43:00 +0530

    Updated

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -35,7 +35,7 @@ export const signUp=async (req,res)=>{
         res.cookie("token",token,{
             httpOnly:true,
             secure:false,
-            sameSite: "Strict",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(user)
