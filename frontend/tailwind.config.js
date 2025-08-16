5a012ddce323826e8ac77847fd5b3a255029b2c8
Abhishek Pandey <abhishek.pandey.2k05@gmail.com>
2025-08-16 22:57:45 +0530

    Final

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        outfit: ["Outfit", "sans-serif"],
+        nunito: ["Nunito Sans", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
