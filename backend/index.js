8a46c16da6b723d1bb63cf12248ea37c745cf680
Abhishek Pandey <abhishek.pandey.2k05@gmail.com>
2025-09-14 18:20:23 +0530

    Deployed

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-powered-lms-1-6aan.onrender.com",
     credentials:true
 }))
 app.use("/api/auth", authRouter)
